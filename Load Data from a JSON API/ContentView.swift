4edc3409b73ddc90425921f38fd50dd5b0959eba
elhajjajiamine <aminselhajjaji@gmail.com>
2021-04-12 00:00:25 +0000

    Initial Commit

---
+++ Load Data from a JSON API/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Load Data from a JSON API
+//
+//  Created by elhajjaji on 12/4/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Load Data from a JSON API/Load_Data_from_a_JSON_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Load_Data_from_a_JSON_APIApp.swift
+//  Load Data from a JSON API
+//
+//  Created by elhajjaji on 12/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct Load_Data_from_a_JSON_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
